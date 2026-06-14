cc009a03fc2212ef418451a2374803ac48275c84
jessiekang37 <145866507+jessiekang37@users.noreply.github.com>
2026-06-14 10:43:57 -0700

    Add: concatenation-of-array - submission-0

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        arr = []
+        n = len(nums)
+        for i in range(len(nums)):
+            #add letter into array
+            arr.append(nums[i])
+        for j in range(len(nums)):
+            arr.append(nums[j])
+        return arr
+        
