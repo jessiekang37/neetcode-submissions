b31d002a0cbe0c970a31b490034681953a9752d4
jessiekang37 <145866507+jessiekang37@users.noreply.github.com>
2026-06-22 18:40:51 -0700

    Add: redistribute-characters-to-make-all-strings-equal - submission-1

---
+++ Data Structures & Algorithms/redistribute-characters-to-make-all-strings-equal/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def makeEqual(self, words: List[str]) -> bool:
+        ans = False
+        #take it out of arr form and put into one str:
+        string = ""
+        
+        for word in words:
+            string += word
+        
+        count = Counter(string)
+        
+        for item in count:
+            if count[item] %len(words) ==0:
+                ans = True
+            else:
+                ans = False
+        return ans
