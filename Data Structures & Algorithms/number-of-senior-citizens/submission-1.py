3d4f99c47d3ac8c786df9b3875a4a0f0d0504dd4
jessiekang37 <145866507+jessiekang37@users.noreply.github.com>
2026-07-01 19:13:27 -0700

    Add: number-of-senior-citizens - submission-1

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-1.py
@@ -0,0 +1,7 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        count = 0
+        for i in range(len(details)):
+                if int((details[i])[11]) > 6 or (int((details[i])[11]) == 6 and int((details[i])[12])> 0 ):
+                    count +=1
+        return count
